23c395b07137d467190a37bd9954e814711f0269
Vladislav Ivanov <vlad@ivanov.email>
2025-09-04 21:12:02 +0200

    test commit

--- libsframe/sframe.c
+++ libsframe/sframe.c
@@ -17,6 +17,8 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+// comment here
+
 #include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
